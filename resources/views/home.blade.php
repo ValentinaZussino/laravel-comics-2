cb9e8cf5da480084669ffc52eff8df851a1744ec
ValentinaZussino <vzussino@protonmail.com>
2022-12-21 00:44:20 +0100

    agg style home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,9 @@
 @extends('layouts.app')
+@section('content')
+    <div class="position-relative">
+        <img src="{{Vite::asset('resources/img/jumbotron.jpg')}}" alt="">
+        <div class="container justify-content-center home-title">
+            <h1>Welcome in <span>DC Comics</span> world</h1>
+        </div>
+    </div>
+@endsection
