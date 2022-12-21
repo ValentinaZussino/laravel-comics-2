f13f7e1685166e7684812c84f336d87e1b5432bd
ValentinaZussino <vzussino@protonmail.com>
2022-12-21 21:47:18 +0100

    sistemaz js per modal delete

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,3 +4,36 @@ import * as bootstrap from 'bootstrap';
 import.meta.glob([
     '../img/**'
 ])
+// Get the modal
+let modalCollection = document.getElementsByClassName("myModal");
+
+// Get the button that opens the modal
+let btnCollection = document.getElementsByClassName("myBtn");
+
+// Get the <span> element that closes the modal
+let spanCollection = document.getElementsByClassName("close");
+
+for(let i=0; i<btnCollection.length; i++){
+
+    // When the user clicks the button, open the modal 
+    btnCollection[i].onclick = function(e) {
+        e.preventDefault(); 
+        modalCollection[i].style.display = "block";
+    }
+    // When the user clicks on <span> (x), close the modal
+    spanCollection[i].onclick = function() {
+        modalCollection[i].style.display = "none";
+    }
+}
+
+
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {     
+  for(let i=0; i<modalCollection.length; i++){         
+    if (event.target == modalCollection[i]) {            
+       modalCollection[i].style.display = "none";        
+       }     
+    } 
+  }
--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -16,13 +16,34 @@
             <div class="btn btn-success mt-5">
                 <a href="{{route('comics.edit', $comic->id)}}" class="text-white text-decoration-none">Update</a>
             </div>
-            <form action="{{route('comics.destroy', $comic->id)}}" method="POST">
+            {{-- <form action="{{route('comics.destroy', $comic->id)}}" method="POST">
                 @csrf
                 @method('DELETE')
                 <div>
                     <button type="submit" class="btn btn-danger mt-5">Delete</button>
                 </div>
-            </form>
+            </form> --}}
+            
+            <!-- Trigger/Open The Modal -->
+            <button id="" class="myBtn">Open Modal</button>
+            <!-- The Modal -->
+            <div id="" class="modal myModal">
+
+                <!-- Modal content -->
+                <div class="modal-content">
+                    <span class="close">&times;</span>
+                    <form action="{{route('comics.destroy', $comic->id)}}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <span class="text-black">Really want to delete {{$comic->title}}</span>
+                        <div>
+                            <button type="submit" class="btn btn-danger mt-5">Delete</button>
+                        </div>
+                    </form> 
+                </div>
+            
+            </div>
+  
         </div>
     </section>
     
